8914d85d3cfd91cf1b7cb3d739e0595be20be14e
felixOlesen <felixolesen@gmail.com>
2026-02-27 23:04:39 +0000

    feature/implemented questions 1.1, 1.2, 1.3 of the cw spec

--- Coursework/cw2/main.cpp
+++ Coursework/cw2/main.cpp
@@ -2,17 +2,41 @@
 #include <thread>
 #include <atomic>
 #include <cstdint>
+#include <random>
 
 class mysem {
   public:
-    mysem(uint32_t init_value){};
-    void acquire(){};
-    void release(){};
+    mysem(uint32_t init_value){ 
+      counter.store(init_value, std::memory_order_seq_cst); 
+    };
+    
+    void acquire(){
+      int current = counter.load(std::memory_order_seq_cst);
+    
+      while (current == 0) {
+        counter.wait(current); 
+        current = counter.load(std::memory_order_seq_cst);
+      }
+
+      counter.fetch_sub(1, std::memory_order_seq_cst);
+    };
+    
+    void release(){ 
+      counter.fetch_add(1, std::memory_order_seq_cst);
+      counter.notify_one();
+    };
+  
   private:
     std::atomic<uint32_t> counter;
 };
 
-void random_work() { };
+void random_work() {
+  int random_amount = rand() % 1001;
+  int count = 0;
+  for (int i = 0; i < random_amount; ++i) {
+    count++;
+  }
+};
 
 int main(int argc, char**argv)
 {
